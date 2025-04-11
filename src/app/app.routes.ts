4d814813b8b5e1aca08dac5f4213c6d666bb8c3f
2z75 <zinedine.chader@gmail.com>
2025-04-11 14:46:29 +0200

    projectCard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ProjectCardComponent },
+];
