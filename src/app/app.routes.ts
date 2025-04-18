20df2846e1d73775be34a8a423f070e389997937
2z75 <zinedine.chader@gmail.com>
2025-04-18 19:54:11 +0200

    contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,5 @@ import { AboutComponent } from './pages/about/about.component';
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'home-page', component: HomePageComponent },
-    { path: 'about', component: AboutComponent}
-
+    { path: 'about', component: AboutComponent},
 ];
