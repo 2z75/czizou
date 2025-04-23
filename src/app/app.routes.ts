4d162c9a1b6b57d6287b025d7df61aeca0627dc5
2z75 <zinedine.chader@gmail.com>
2025-04-23 16:41:39 +0200

    finish static

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PlayComponent } from './pages/play/play.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'projects', component: HomePageComponent },
     { path: 'about', component: AboutComponent},
+    { path: 'play', component: PlayComponent}
 ];
