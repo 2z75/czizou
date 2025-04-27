c6ddc60ef98dacadd9ac0e9b74f070819ab8d35c
2z75 <zinedine.chader@gmail.com>
2025-04-27 21:37:36 +0200

    carrousel animation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProjectsComponent } from './pages/project/projects.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PlayComponent } from './pages/play/play.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
-    { path: 'projects', component: HomePageComponent },
+    { path: 'projects', component: ProjectsComponent },
     { path: 'about', component: AboutComponent},
     { path: 'play', component: PlayComponent}
 ];
