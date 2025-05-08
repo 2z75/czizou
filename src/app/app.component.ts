ac42e2d8f253cf042ef3d2fbaaafd37582fe3847
2z75 <zinedine.chader@gmail.com>
2025-05-08 02:54:06 +0200

    add threeJS models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd, NavigationStart } from '@angular/router';
 import { filter, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import gsap from 'gsap';
 
 @Component({
     selector: 'app-root',
@@ -11,7 +12,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnDestroy, AfterViewInit {
+    @ViewChild('pageWrapper', { static: true }) pageWrapper!: ElementRef;
 
     title = 'czizou';
     showNavbar = true;
@@ -22,6 +24,26 @@ export class AppComponent implements OnDestroy {
         .pipe(filter(event => event instanceof NavigationEnd))
         .subscribe((event: NavigationEnd) => {
             this.showNavbar = !['/'].includes(event.urlAfterRedirects);
+            });
+    }
+
+    ngAfterViewInit(): void {
+        this.setupTransition()
+    }
+
+    private setupTransition(): void {
+        this.router.events
+        .pipe(filter(event => event instanceof NavigationStart || event instanceof NavigationEnd))
+        .subscribe(event => {
+            const wrapper = this.pageWrapper.nativeElement;
+
+            if (event instanceof NavigationStart) {
+                gsap.to(wrapper, { opacity: 0, scale:0.98, duration: 0.6, ease: 'power3.inOut' });
+            }
+
+            if (event instanceof NavigationEnd) {
+                gsap.fromTo(wrapper, { opacity: 0, scale: 0.98 }, { opacity: 1, scale: 1, duration: 0.7, ease: 'power3.out', delay: 0.1 });
+            }
         });
     }
 
